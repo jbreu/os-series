5a02f9be786c2b79b7f435bef0dafe0e86ac44ef
jbreu <jakobbreu@googlemail.com>
2024-04-14 14:33:43 +0200

    init

--- kernel/src/kernel.rs
+++ kernel/src/kernel.rs
@@ -1,6 +1,13 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+extern crate alloc;
+
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
 use core::{arch::asm, panic::PanicInfo};
 
 mod gdt;
@@ -23,10 +30,28 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn init_heap() {
+    extern "C" {
+        static mut KERNEL_END: *const u64;
+    }
+
+    unsafe {
+        let heap_start = KERNEL_END;
+        kprintln!("heap start: {:x}\n", heap_start as u64);
+        //let heap_end = …;
+
+        //let heap_size = heap_end - heap_start;
+
+        //ALLOCATOR.lock().init(heap_start, heap_size);
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn kernel_main() -> ! {
     gdt::init_gdt();
     interrupt::init_idt();
+
+    init_heap();
 
     clear_console!();
     kprintln!("successfull boot!");
