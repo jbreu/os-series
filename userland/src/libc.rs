9186c193afa47252289043449c211ee69e229467
jbreu <jakobbreu@googlemail.com>
2024-06-15 17:35:50 +0200

    skeletons

--- userland/src/libc.rs
+++ userland/src/libc.rs
@@ -1,4 +1,4 @@
-use core::arch::asm;
+use core::{arch::asm, array};
 
 //pid_t getppid(void);
 
@@ -103,6 +103,39 @@ pub fn malloc(size: usize) -> u64 {
     }
 
     return address;
+}
+
+pub fn free(address: u64) {
+    // TODO implement working free; problem is that LockedHeap::dealloc requires the size...
+    // TODO Implement; not doing anything for now
+}
+
+pub fn open(filename: &str, mode: &str) -> u64 {
+    // TODO Implement; not doing anything for now
+}
+
+pub fn close(handle: u64) {
+    // TODO Implement; not doing anything for now
+}
+
+pub fn read(handle: u64, count: usize) -> array<u8> {
+    // FIXME needed
+}
+
+pub fn write(handle: u64, buffer: array<u8>, count: usize) -> buffer {
+    // FIXME needed
+}
+
+pub fn seek(handle: u64, offset: u64, origin: u64) -> usize {
+    // FIXME needed
+}
+
+pub fn tell(handle: u64) -> usize {
+    // FIXME needed
+}
+
+pub fn eof(handle: u64) -> usize {
+    // FIXME needed
 }
 
 pub struct Printer {}
