1099a0a85fee57fba3f29c9716f7f981d7058a22
jbreu <20864561+jbreu@users.noreply.github.com>
2024-11-10 20:19:29 +0100

    get a bit further

--- userland/usr/include/ctype.h
+++ userland/usr/include/ctype.h
@@ -3,18 +3,3 @@ typedef unsigned char uint8_t;
 typedef unsigned long uint32_t;
 typedef unsigned long long uint64_t;
 typedef unsigned long long size_t;
-
-int	isalnum(int);
-int	isalpha(int);
-int	iscntrl(int);
-int	isdigit(int);
-int	isgraph(int);
-int	islower(int);
-int	isprint(int);
-int	ispunct(int);
-int	isspace(int);
-int	isupper(int);
-int	isxdigit(int);
-int	tolower(int);
-int	toupper(int);
-int	isblank(int);
---
+++ userland/usr/include/string.h
@@ -0,0 +1,11 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include "ctype.h"
+#include "stdbool.h"
+
+// String and memory functions
+uint64_t strlen( const char* str );
+bool strcmp(const char* a, const char* b);
+
+#endif
